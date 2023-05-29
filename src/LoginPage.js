2745dd4c21c4da9c6df6edb33e8feecde5f3f5cd
Omer Halfon <omerhalfon1234@gmail.com>
2023-05-29 18:39:49 +0300

    add excel file

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import {utils as XLSXUtils, writeFile as writeXLSXFile} from 'xlsx';
+// eslint-disable-next-line max-len
+import {utils as XLSXUtils, readFile as readXLSXFile, writeFile as writeXLSXFile} from 'xlsx';
 import './LoginPage.css';
+
 const LoginPage = () => {
   const [name, setName] = useState('');
   const [grade1, setGrade1] = useState('');
@@ -8,8 +10,9 @@ const LoginPage = () => {
   const [grade3, setGrade3] = useState('');
   const [Response, setResponse] = useState('');
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
     // eslint-disable-next-line max-len
     if (grade1 < 0 || grade1 > 100 || grade2 < 0 || grade2 > 100 || grade3 < 0 || grade3 > 100) {
       setResponse('Grade should be between 0 to 100');
@@ -20,20 +23,16 @@ const LoginPage = () => {
     } else {
       setResponse('');
     }
-    // Create a new workbook
-    const workbook = XLSXUtils.book_new();
+    const existingWorkbook = await readXLSXFile('students_data.xlsx');
+    const sheetName = existingWorkbook.SheetNames[0];
+    const sheet = existingWorkbook.Sheets[sheetName];
 
-    // Create a new worksheet
-    const worksheet = XLSXUtils.aoa_to_sheet([
-      ['Name', 'Grade 1', 'Grade 2', 'Grade 3'],
-      [name, grade1, grade2, grade3],
-    ]);
+    // eslint-disable-next-line max-len
+    XLSXUtils.sheet_add_json(sheet, [{'Name': name, 'Grade 1': grade1, 'Grade 2': grade2, 'Grade 3': grade3}], {skipHeader: true});
 
-    // Add the worksheet to the workbook
-    XLSXUtils.book_append_sheet(workbook, worksheet, 'Login Data');
+    await writeXLSXFile(existingWorkbook, 'students_data.xlsx');
 
-    // Save the workbook as an Excel file
-    writeXLSXFile(workbook, 'login_data.xlsx');
+    setResponse('Data added successfully!');
   };
 
   return (
@@ -41,7 +40,7 @@ const LoginPage = () => {
       <h1>Login Page</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label className= "fonts">Name:</label>
+          <label className="fonts">Name:</label>
           <input
             type="text"
             id="name"
@@ -51,7 +50,7 @@ const LoginPage = () => {
           />
         </div>
         <div>
-          <label className= "fonts">Grade 1:</label>
+          <label className="fonts">Grade 1:</label>
           <input
             type="text"
             id="grade1"
@@ -59,10 +58,9 @@ const LoginPage = () => {
             onChange={(event) => setGrade1(event.target.value)}
             required
           />
-
         </div>
         <div>
-          <label className= "fonts">Grade 2:</label>
+          <label className="fonts">Grade 2:</label>
           <input
             type="text"
             id="grade2"
@@ -72,7 +70,7 @@ const LoginPage = () => {
           />
         </div>
         <div>
-          <label className= "fonts">Grade 3:</label>
+          <label className="fonts">Grade 3:</label>
           <input
             type="text"
             id="grade3"
@@ -81,12 +79,8 @@ const LoginPage = () => {
             required
           />
         </div>
-
         <button type="submit">Submit</button>
-        <p className="fonts">
-          {Response}
-        </p>
-
+        <p className="fonts">{Response}</p>
       </form>
     </div>
   );
